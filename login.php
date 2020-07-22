835f06fc4ae3e1f20ec3c811802bff359344be5c
SaydaSakila <16303029@iubat.edu>
2020-07-22 21:00:48 +0600

    Design Login page with HTML

--- login.php
+++ login.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link href="assets/css/login.css" rel="stylesheet">
+
 </head>
 <body>
     <div class="login-wrapper">
@@ -11,17 +13,17 @@
             <div class="table-cell">
                 <div class="login-box">
                     <div class="login-header">
-                        <h2>ADMIN LOGIN</h2>
+                        <h2 style="color: #263baf">ADMIN LOGIN</h2>
                     </div>
                     <div class="login-body">
                         <form action="submit/login-submit.php" method="POST">
                             <div class="form-group">
                                 <label for="">Email or Username</label>
-                                <input type="text" name="email" class="form-control" placeholder="Enter email or username">
+                                <input type="text" name="email" class="form-control" placeholder="Enter Your Email or Username">
                             </div>
                             <div class="form-group">
                                 <label for="">Password</label>
-                                <input type="password" name="password" class="form-control" placeholder="Enter password">
+                                <input type="password" name="password" class="form-control" placeholder="Enter Your Password">
                             </div>
                             <div class="form-group">
                                 <input type="submit" name="login_submit" class="login-btn" value="LOGIN">
